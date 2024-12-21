09d2480ffd7aa28718113428c7e1f305df4527e3
barry <newTest@gmail.com>
2024-12-22 01:24:25 +0800

    error

--- app/deleteTableBlogs/route.ts
+++ app/deleteTableBlogs/route.ts
@@ -1,26 +1,18 @@
 // DELETE
 import { db } from "@vercel/postgres";
+
 const client = await db.connect();
 
-export async function deleteBlogsTable() {
-  // throw new Error("Failed to Delete Invoice");
-
-  //   await sql`DELETE FROM invoices WHERE id = ${id}`;
-  //   //   revalidatePath("/dashboard/invoices");
-
+async function deleteBlogsTable() {
   await client.sql`DROP TABLE IF EXISTS blogs`;
 }
 
 export async function GET() {
   try {
-    await client.sql`BEGIN`;
     await deleteBlogsTable();
-    await client.sql`COMMIT`;
-
     return Response.json({ message: "table blogs deleted" });
   } catch (error) {
     const err = error as Error;
-    await client.sql`ROLLBACK`;
     return Response.json({ error: err.message });
   } finally {
     client.release();
