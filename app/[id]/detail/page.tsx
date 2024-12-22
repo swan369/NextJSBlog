9d8fe1d5854eff08d5eb4b75937846b4fa2d0614
barry <newTest@gmail.com>
2024-12-22 22:08:23 +0800

    unused variables removed

--- app/[id]/detail/page.tsx
+++ app/[id]/detail/page.tsx
@@ -3,9 +3,7 @@ import { fetchFinBlogById } from "@/app/lib/fetch";
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
-  const { title, author, imageurl, detail, date } = (
-    await fetchFinBlogById(id)
-  )[0];
+  const { title, imageurl, detail } = (await fetchFinBlogById(id))[0];
 
   return (
     <>
