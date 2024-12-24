057d844d01c7b743c3094ecea5828c84153ea85d
barry <newTest@gmail.com>
2024-12-25 04:38:34 +0800

    new state

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -1,3 +1,4 @@
+// import { sql } from "@vercel/postgres";
 import { sql } from "@vercel/postgres";
 
 export async function fetchAllBlogs() {
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,6 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { useSearch } from "../lib/provider";
-import { Suspense } from "react";
 
 export function SearchBlog() {
   const searchParams = useSearchParams();
