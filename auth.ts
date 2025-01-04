7ee3543fec559a7519679a64c8fb8d01312034ed
barry <newTest@gmail.com>
2025-01-05 01:17:53 +0800

    bug

--- auth.ts
+++ auth.ts
@@ -27,6 +27,7 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  secret: process.env.AUTH_SECRET,
   // providers: [] replaced by :
   providers: [
     Credentials({
